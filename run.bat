849873fa0b8ffb5cbd2dc5fb6638a4c155a1df20
Deniz Şafak <denizsafak98@gmail.com>
2025-01-08 13:30:23 +0300

    v3.1

--- run.bat
+++ run.bat
@@ -5,14 +5,63 @@ set NAME=AutoSubSync
 set PROJECTFOLDER=main
 set RUN=%PROJECTFOLDER%\AutoSubSync.pyw
 set requirementsFile=%PROJECTFOLDER%\requirements.txt
-set VENV_PATH=.venv
-set ACTIVATE_PATH=%VENV_PATH%\Scripts\activate
 set CURRENT_DIR=%CD%
 set LAST_DIR_FILE=%PROJECTFOLDER%\last_known_directory.txt
 set refrenv=%PROJECTFOLDER%\refrenv.bat
-set PYTHON_DOWNLOAD_URL=https://www.python.org/ftp/python/3.13.0/python-3.13.0-amd64.exe
-set FFSUBSYNC_MIN_PYTHON_VERSION="3.6"
-set FFSUBSYNC_MAX_PYTHON_VERSION="3.12"
+set PYTHON_PATH=python_embedded\pythonw.exe
+set PYTHON_EMBEDDED_URL=https://github.com/wojiushixiaobai/Python-Embed-Win64/releases/download/3.12.8/python-3.12.8-embed-amd64.zip
+set PYTHON_EMBEDDED_FILE=%PROJECTFOLDER%\python_embedded.zip
+
+:: Check if python_embedded directory exists
+if not exist "python_embedded" (
+    if not exist %PYTHON_EMBEDDED_FILE% (
+        echo Downloading python_embedded.zip...
+        curl -L -o %PYTHON_EMBEDDED_FILE% %PYTHON_EMBEDDED_URL%
+        if errorlevel 1 (
+            echo Failed to download embedded Python with curl. Trying with PowerShell method...
+            powershell -Command "Invoke-WebRequest -Uri %PYTHON_EMBEDDED_URL% -OutFile %PYTHON_EMBEDDED_FILE%"
+            if errorlevel 1 (
+                echo Failed to download embedded Python. Please check your internet connection and try again.
+                pause
+                exit /b
+            )
+        )
+    )
+    echo Creating python_embedded directory...
+    mkdir python_embedded
+    if errorlevel 1 (
+        echo Failed to create python_embedded directory.
+        pause
+        exit /b
+    )
+    echo Unzipping python_embedded.zip...
+    tar -xf %PYTHON_EMBEDDED_FILE% -C python_embedded
+    if errorlevel 1 (
+        echo Failed to unzip embedded Python with tar. Trying with PowerShell method...
+        powershell -Command "Expand-Archive -Path %PYTHON_EMBEDDED_FILE% -DestinationPath python_embedded"
+        if errorlevel 1 (
+            echo Failed to unzip embedded Python.
+            pause
+            exit /b
+        )
+    )
+    ::del %PYTHON_EMBEDDED_FILE%
+    echo Editing python312._pth file...
+    powershell -Command "(Get-Content 'python_embedded\\python312._pth') -replace '#import site', 'import site' | Set-Content 'python_embedded\\python312._pth'"
+    if errorlevel 1 (
+        echo Failed to edit python312._pth file. Trying alternative method...
+        (
+            echo import site
+            type python_embedded\python312._pth | findstr /v "#import site"
+        ) > python_embedded\python312_temp._pth
+        move /Y python_embedded\python312_temp._pth python_embedded\python312._pth
+        if errorlevel 1 (
+            echo Failed to edit python312._pth file. Please edit the file manually and try again. You need to replace '#import site' with 'import site'. You can find the file in python_embedded directory. After editing, please run this script again.
+            pause
+            exit /b
+        )
+    )
+)
 
 :: Display provided argument if any
 if not "%~1"=="" (
@@ -20,78 +69,8 @@ if not "%~1"=="" (
 )
 
 :: Check if the required programs are installed
-pip --version >nul 2>&1 && (set python_installed=true) || (set python_installed=false)
+%PYTHON_PATH% --version >nul 2>&1 && (set python_installed=true) || (set python_installed=false)
 ffmpeg -version >nul 2>&1 && (set ffmpeg_installed=true) || (set ffmpeg_installed=false)
-
-:: Check if Python and ffmpeg are installed
-if not "%python_installed%"=="true" (
-    if not "%ffmpeg_installed%"=="true" (
-        echo Python and ffmpeg are required to run this program.
-        set /p "install_both=Do you want to install Python and ffmpeg (y/n)?: "
-        if /i "!install_both!"=="y" (
-            echo Installing Python and ffmpeg...
-            winget install python ffmpeg --accept-package-agreements
-            if errorlevel 1 (
-                echo Failed to install Python or ffmpeg using winget. Trying to install Python manually...
-                goto bothwingetfailed
-            )
-            set python_installed=true
-            set ffmpeg_installed=true
-            :: Refresh the environment variables
-            call %refrenv%
-        ) else (
-            echo Both Python and ffmpeg are required to run this program. Please install them and try again.
-            pause
-            exit /b
-        )
-    )
-)
-
-if not "%python_installed%"=="true" (
-    echo Python is required to run this program.
-    set /p "userinp=Do you want to install Python (y/n)?: "
-    if /i "!userinp!"=="y" (
-        echo Downloading Python...
-        winget install python --accept-package-agreements
-        if errorlevel 1 (
-            echo Download failed using winget method. Trying to download manually...
-            :bothwingetfailed
-            if not exist "%PROJECTFOLDER%\python_installer.exe" (
-                echo Downloading Python from %PYTHON_DOWNLOAD_URL%
-                curl %PYTHON_DOWNLOAD_URL% -o %PROJECTFOLDER%\python_installer.exe
-                if not exist "%PROJECTFOLDER%\python_installer.exe" (
-                    echo Download failed using curl. Trying with the powershell method
-                    powershell -Command "& {Invoke-WebRequest -Uri '%PYTHON_DOWNLOAD_URL%' -OutFile '%PROJECTFOLDER%\python_installer.exe'}"
-                    if errorlevel 1 (
-                        echo Failed to install Python. Please install Python manually and try again.
-                        pause
-                        exit /b
-                    )
-                )
-            )
-            echo Installing Python. Please wait...
-            start /wait %PROJECTFOLDER%\python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-            if errorlevel 1 (
-                echo Failed to install Python. Please install Python manually and try again.
-                pause
-                exit /b
-            )
-            del %PROJECTFOLDER%\python_installer.exe
-            goto python_ok
-        )
-    ) else (
-        echo Python is required to run this program. Please install Python and try again.
-        pause
-        exit /b
-    )
-    :python_ok
-    echo Python installed successfully.
-    :: Refresh the environment variables
-    call %refrenv%
-    set python_installed=true
-)
-
-
 
 :: Check if ffmpeg is installed
 if not "%ffmpeg_installed%"=="true" (
@@ -132,37 +111,8 @@ if not "%ffmpeg_installed%"=="true" (
     )
     :ffmpeg_ok
     echo ffmpeg installed successfully.
-    set python_installed=true
     :: Refresh the environment variables
     call %refrenv%
-)
-
-:: Check if virtual environment exists
-IF NOT EXIST %VENV_PATH% (
-    echo Creating virtual environment...
-    echo %CURRENT_DIR% > "%LAST_DIR_FILE%"
-    python -m venv %VENV_PATH%
-) ELSE (
-    goto check_python
-)
-
-:: Check if Python exists at the path stored in the virtual environment
-set count_python=0
-:check_python
-%VENV_PATH%\Scripts\python.exe --version >nul 2>&1
-if %errorlevel% neq 0 (
-    set /a count_python+=1
-    echo Python not found at the path stored in the virtual environment.
-    echo Recreating virtual environment...
-    rmdir /s /q %VENV_PATH%
-    python -m venv %VENV_PATH%
-    if !count_python! lss 3 (
-        goto check_python
-    ) else (
-        echo Something went wrong. Please try again.
-        pause
-        exit /b
-    )
 )
 
 :: Check if the current directory has changed since the last run
@@ -172,15 +122,16 @@ if exist "%LAST_DIR_FILE%" (
         if not exist "!last_dir!" (
             echo Current directory: %CURRENT_DIR%
             echo Last known directory: !last_dir!
-            echo Looks like the directory of the program has been changed. 
-            echo It is hihgly recommended to recreate the virtual environment.
-            set /p "recreate_venv=Recreate the virtual environment (y/n)?: "
-            if /i "!recreate_venv!"=="y" (
-                echo Recreating virtual environment...
-                rmdir /s /q %VENV_PATH%
-                python -m venv %VENV_PATH%
-				:: Update the last directory file
-				echo %CURRENT_DIR% > "%LAST_DIR_FILE%"
+            echo Looks like the directory of the program has been changed.
+            :: Ask user if they want to reinstall requirements
+            set /p choice="ffsubsync might not work. Do you want to re-install ffsubsync? (y/n): "
+            if /i "!choice!"=="y" (
+                echo Uninstalling ffsubsync...
+                %PYTHON_PATH% -m pip uninstall ffsubsync -y
+                echo Installing ffsubsync...
+                %PYTHON_PATH% -m pip install ffsubsync --quiet
+                :: Update the last directory file
+                echo %CURRENT_DIR% > "%LAST_DIR_FILE%"
             )
         )
     )
@@ -189,106 +140,51 @@ if exist "%LAST_DIR_FILE%" (
     echo %CURRENT_DIR% > "%LAST_DIR_FILE%"
 )
 
-:: Activate the virtual environment
-set count_activate=0
-:activate
-echo Activating virtual environment...
-call %ACTIVATE_PATH%
-if errorlevel 1 (
-    set /a count_activate+=1
-    echo Failed to activate virtual environment.
-    echo Recreating virtual environment...
-    rmdir /s /q %VENV_PATH%
-    python -m venv %VENV_PATH%
-    if !count_activate! lss 3 (
-        goto activate
-    ) else (
-        echo Failed to activate virtual environment. Please try again.
-        pause
-        exit /b
-    )
-)
-
-:: Check if tkinter is installed
-%VENV_PATH%\Scripts\python.exe -c "import tkinter" 2>nul
-if errorlevel 1 (
-    echo tkinter is not installed. Installing tkinter...
-    pip install tk
-    if errorlevel 1 (
-        echo Failed to install tkinter. Please check your internet connection and try again.
-        pause
-        exit /b
-    )
-)
-
-:: Check Python version for ffsubsync
-for /f "tokens=2 delims= " %%i in ('python --version') do set PYTHON_VERSION=%%i
-for /f "tokens=1,2 delims=." %%a in ("%PYTHON_VERSION%") do (
-    set PYTHON_MAJOR=%%a
-    set PYTHON_MINOR=%%b
-)
-if not defined PYTHON_MINOR (
-    set PYTHON_MINOR=0
-)
-for /f "tokens=* delims=0" %%i in ("%PYTHON_MAJOR%") do set PYTHON_MAJOR=%%i
-for /f "tokens=* delims=0" %%i in ("%PYTHON_MINOR%") do set PYTHON_MINOR=%%i
-set /a PYTHON_VERSION_NUMBER=%PYTHON_MAJOR%*100 + %PYTHON_MINOR%
-
-for /f "tokens=1,2 delims=." %%a in (%FFSUBSYNC_MIN_PYTHON_VERSION%) do (
-    set MIN_RECOMMENDED_MAJOR=%%a
-    set MIN_RECOMMENDED_MINOR=%%b
-)
-set /a MIN_RECOMMENDED_VERSION_NUMBER=%MIN_RECOMMENDED_MAJOR%*100 + %MIN_RECOMMENDED_MINOR%
-
-for /f "tokens=1,2 delims=." %%a in (%FFSUBSYNC_MAX_PYTHON_VERSION%) do (
-    set MAX_RECOMMENDED_MAJOR=%%a
-    set MAX_RECOMMENDED_MINOR=%%b
-)
-set /a MAX_RECOMMENDED_VERSION_NUMBER=%MAX_RECOMMENDED_MAJOR%*100 + %MAX_RECOMMENDED_MINOR%
-
-if %PYTHON_VERSION_NUMBER% lss %MIN_RECOMMENDED_VERSION_NUMBER% (
-    echo.
-    echo Warning: Your Python version is %PYTHON_VERSION%.
-    echo ffsubsync requires Python 3.6 or higher.
-    echo Please install Python 3.6 or higher.
-    echo.
-) else if %PYTHON_VERSION_NUMBER% gtr %MAX_RECOMMENDED_VERSION_NUMBER% (
-    echo.
-    echo Warning: Your Python version is %PYTHON_VERSION%.
-    echo ffsubsync might give an error when installing.
-    echo If that happens, please install Python 3.12 or lower.
-    echo.
-)
-
 :: Get the list of installed packages using pip freeze
 echo Checking the requirements...
 :: python.exe -m pip install --upgrade pip --quiet
-for /f "tokens=1,* delims==" %%i in ('pip freeze --all') do (
+for /f "tokens=1,* delims==" %%i in ('%PYTHON_PATH% -m pip freeze --all') do (
     set installed[%%i]=%%j
 )
 
 :: Compare with the requirements from the requirements.txt file
-for /f "tokens=1,* delims==" %%i in (%requirementsFile%) do (
-    if not "!installed[%%i]!"=="%%j" (
-        echo Installing package: %%i==%%j
-        pip install %%i==%%j --upgrade --quiet
-        if errorlevel 1 (
-            echo Failed to install %%i==%%j. Please check your internet connection and try again.
-            pause
-            exit /b
-        )
-    )
-)
+call :install_requirements
 
 :: Run the program
 echo Starting %NAME%...
-start /B "" "%VENV_PATH%\Scripts\pythonw.exe" %RUN% %* > nul 2>&1
+start /B "" "%PYTHON_PATH%" %RUN% %* > nul 2>&1
 if errorlevel 1 (
     echo Failed to start %NAME%. Please try again.
     pause
     exit /b
 )
 
+exit /b
+
+:install_requirements
+for /f "tokens=1,* delims==" %%i in (%requirementsFile%) do (
+    if "%%j"=="" (
+        if not defined installed[%%i] (
+            echo Installing latest package: %%i
+            %PYTHON_PATH% -m pip install %%i --upgrade --quiet
+            if errorlevel 1 (
+                echo Failed to install %%i. Please check your internet connection and try again.
+                pause
+                exit /b
+            )
+        )
+    ) else (
+        if not "!installed[%%i]!"=="%%j" (
+            echo Installing package: %%i==%%j
+            %PYTHON_PATH% -m pip install %%i==%%j --upgrade --quiet
+            if errorlevel 1 (
+                echo Failed to install %%i==%%j. Please check your internet connection and try again.
+                pause
+                exit /b
+            )
+        )
+    )
+)
 exit /b
 
 :check_Permissions
