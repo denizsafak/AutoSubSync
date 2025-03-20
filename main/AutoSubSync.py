84385add16e3a383fdbb2a868f79f20394fa0d03
Deniz Şafak <denizsafak98@gmail.com>
2025-03-20 19:19:38 +0300

    fix "Unable to detect speech"

--- main/AutoSubSync.py
+++ main/AutoSubSync.py
@@ -44,26 +44,34 @@ def get_base_dir():
     return base_dir
 
 base_dir = get_base_dir()
+program_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Set the working directory to the script's directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# Define paths to the executables
-ffmpeg_bin = os.path.join(os.curdir, "resources", "ffmpeg-bin")
-alass_bin = os.path.join(os.curdir, "resources", "alass-bin")
-ffsubsync_bin = os.path.join(os.curdir, "resources", "ffsubsync-bin")
+# Define paths to the executables - FIXED
+ffmpeg_bin = os.path.join(program_dir, "resources", "ffmpeg-bin")
+alass_bin = os.path.join(program_dir, "resources", "alass-bin")
+ffsubsync_bin = os.path.join(program_dir, "resources", "ffsubsync-bin")
+
 # Add the paths to the system PATH environment variable
-os.environ["PATH"] += (
-    os.pathsep + ffmpeg_bin + os.pathsep + alass_bin + os.pathsep + ffsubsync_bin
+os.environ["PATH"] = (
+    ffmpeg_bin
+    + os.pathsep
+    + alass_bin
+    + os.pathsep
+    + ffsubsync_bin
+    + os.pathsep
+    + os.environ["PATH"]
 )
 
 # Determine correct alass executable based on platform
 if platform in ("Windows", "Darwin"):
-    CALL_ALASS = "alass-cli"
+    CALL_ALASS = os.path.join(alass_bin, "alass-cli")
 elif platform == "Linux":
-    CALL_ALASS = "alass-linux64"
+    CALL_ALASS = os.path.join(alass_bin, "alass-linux64")
 else:
-    CALL_ALASS = "alass"  # fallback
+    CALL_ALASS = os.path.join(alass_bin, "alass")  # fallback
 
 # Determine correct ffmpeg, ffprobe, and ffsubsync executables based on platform
 if platform == "Windows":
