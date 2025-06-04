87e445ed6145cb5fdac1700ae9c4169f7ddd0831
Deniz Şafak <denizsafak98@gmail.com>
2025-06-04 22:08:43 +0300

    Fix again

--- main/texts.py
+++ main/texts.py
@@ -3,7 +3,10 @@
 # You can contribute in GitHub by forking the repository and making a pull request. #
 #####################################################################################
 import os
+import sys
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # Add parent directory
+sys.path.append((os.path.dirname(__file__)))  # Add current directory
 
 from functions.get_platform import platform
 
